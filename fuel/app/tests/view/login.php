26ede76e3ab1eb4a2e2ec6a0962c849759ecee15
Vo Yen <vo.yen@mulodo.com>
2013-11-29 17:20:54 +0700

    aaaa

--- fuel/app/tests/view/login.php
+++ fuel/app/tests/view/login.php
@@ -8,8 +8,11 @@ class Login extends PHPUnit_Extensions_Selenium2TestCase
 {
     protected function setUp()
     {
+    	$this->setHost('localhost');
+    	$this->setPort(4444);
         $this->setBrowser('firefox');
-        $this->setBrowserUrl('http://localhost:8080/index.php/');
+        $this->setBrowserUrl('http://localhost/testgit/htdocs/');
+       // $this->setBrowserUrl('http://localhost:8080/index.php/');
     }
  
     public function testTitle()
