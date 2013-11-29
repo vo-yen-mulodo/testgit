164ed5d5d69df2d4d00841ae57bfa9518e7f8c57
Vo Yen <vo.yen@mulodo.com>
2013-11-29 17:37:56 +0700

    ajbkjscba

--- fuel/app/tests/view/login.php
+++ fuel/app/tests/view/login.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 /**
  * @group login
  * @author voyen1602
@@ -8,9 +8,8 @@ class Login extends PHPUnit_Extensions_Selenium2TestCase
 {
     protected function setUp()
     {
-    	$this->setHost('localhost');
         $this->setBrowser('firefox');
-     //   $this->setBrowserUrl('http://localhost/testgit/htdocs/');
+     //   $this->setBrowserUrl('http://localhost:8888/testgit/htdocs/');
         $this->setBrowserUrl('http://localhost:8080/index.php/');
     }
  
