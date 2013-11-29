68078aea7dfbb9eb4e763ef056973d85bbb47f2f
Vo Yen <vo.yen@mulodo.com>
2013-11-29 16:15:30 +0700

    update config travis

--- setup_fpm_and_nginx_for_travis.sh
+++ setup_fpm_and_nginx_for_travis.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+apt-get update
 apt-get install nginx
-apt-get install php5-fpm
+apt-get install php5-fpm php5-cli php5-mysql -y
+apt-get install php5-gd
 cp .travis_nginx.conf /etc/nginx/nginx.conf
 /etc/init.d/nginx restart
+chmod 777 /home/travis/build/mulodo-vietnam/fow_api/fuel/app/logs
+#cat /etc/php5/fpm/php.ini
+mv /etc/php5/fpm/php.ini php.ini.old
+cp /home/travis/.phpenv/versions/5.3.3/etc/php.ini /etc/php5/fpm/php.ini
+/etc/init.d/php5-fpm restart
+find / -name php.ini
